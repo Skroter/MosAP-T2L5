9d4b35cb981ea3b93e403926eb995f879f80153e
Skroter <digro@yandex.ru>
2024-06-27 19:15:13 +0500

    Добавьте файлы проекта.

---
+++ Topic2Lesson5Task2/Topic2Lesson5Task2.py
@@ -0,0 +1,20 @@
+﻿word = input('Введите слово из строчныч латинских букв: ');
+c = ""
+vowel = 0
+consonant = 0
+x1 = True
+for a in word:
+    if a not in c:
+        if a in 'aeoiu':
+            print(f"{a}: {word.count(a)}", 'глассная' )
+            c += a
+for d in word:
+    if d in 'aeuio':
+        vowel += 1
+    else:
+        consonant += 1
+        x1 = False
+if x1 == False:
+    print('Какой то из букв условия нет в слове', x1)
+print('Гласных ', vowel)
+print('Согласных ', consonant)
---
+++ Topic2Lesson5Task3/Topic2Lesson5Task3.py
@@ -0,0 +1,15 @@
+﻿summInvestments = 1000
+cashIvan = int(input('Сколько денег у Ивана: '))
+cashMike = int(input('Сколько денег у Майла: '))
+
+if cashIvan >= summInvestments <= cashMike:
+    print(2)
+elif cashMike < summInvestments <= cashIvan:
+    print('Ivan')
+elif cashIvan < summInvestments <= cashMike:
+    print('Mike')
+if cashIvan < summInvestments > cashMike:
+    if cashIvan+cashMike >= summInvestments:
+        print('1')
+if cashIvan+cashMike < summInvestments:
+    print('0')
